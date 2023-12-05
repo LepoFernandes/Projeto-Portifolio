8ea4748a115e359bcc300c5d88629ad617087740
Lucas Fernandes <142260544+LepoFernandes@users.noreply.github.com>
2023-12-05 12:29:50 -0300

    a

--- src/js/script.js
+++ src/js/script.js
@@ -13,3 +13,22 @@ icons.forEach(icon => {
         tooltip.style.display = 'none';
     });
 });
+
+
+function escreverTexto(elemento, texto, intervalo) {
+    let i = 0;
+    const escrever = setInterval(function() {
+      elemento.innerHTML += texto.charAt(i);
+      i++;
+      if (i > texto.length) {
+        clearInterval(escrever);
+      }
+    }, intervalo);
+  } 
+  document.getElementById("h1-home").style.display = "none";
+  const elementoTexto = document.getElementById('texto-escrevendo');
+  const textoParaEscrever = 'LUCAS FERNANDES';
+  const intervaloDeEscrita = 100;
+  escreverTexto(elementoTexto, textoParaEscrever, intervaloDeEscrita);
+
+  
