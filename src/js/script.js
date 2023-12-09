aa06c94d9546f8626cef44a8b3409253facaa4ec
Lucas Fernandes <142260544+LepoFernandes@users.noreply.github.com>
2023-12-09 13:26:57 -0300

    update

--- src/js/script.js
+++ src/js/script.js
@@ -1,28 +1,26 @@
-
 function escreverTexto(elemento, texto, intervalo) {
-    let i = 0;
-    const escrever = setInterval(function() {
-      elemento.innerHTML += texto.charAt(i);
-      i++;
-      if (i > texto.length) {
-        clearInterval(escrever);
-      }
-    }, intervalo);
-  } 
-  document.getElementById("h1-home").style.display = "none";
-  const elementoTexto = document.getElementById('texto-escrevendo');
-  const textoParaEscrever = 'LUCAS FERNANDES';
-  const intervaloDeEscrita = 100;
-  escreverTexto(elementoTexto, textoParaEscrever, intervaloDeEscrita);
-
+  let i = 0;
+  const escrever = setInterval(function() {
+    elemento.innerHTML += texto.charAt(i);
+    i++;
+    if (i > texto.length) {
+      clearInterval(escrever);
+    }
+  }, intervalo);
+} 
+document.getElementById("h1-home").style.display = "none";
+const elementoTexto = document.getElementById('texto-escrevendo');
+const textoParaEscrever = 'LUCAS FERNANDES';
+const intervaloDeEscrita = 100;
+escreverTexto(elementoTexto, textoParaEscrever, intervaloDeEscrita);
+  
 
 let count = 1;
 document.getElementById("radio1").checked = true;
 
 setInterval(function(){
   nextImage();
-},5000)
-
+},6000)
 
 function nextImage(){
   count++;
@@ -33,6 +31,7 @@ function nextImage(){
 
   document.getElementById("radio"+count).checked = true;
 }
+
 
 
 document.addEventListener('DOMContentLoaded', function () {
