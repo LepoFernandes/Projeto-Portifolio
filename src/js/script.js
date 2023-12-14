9215a1cd05e5f17b7df26f42bc6788f651136f1d
Lucas Fernandes <142260544+LepoFernandes@users.noreply.github.com>
2023-12-14 15:40:01 -0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -11,7 +11,7 @@ function escreverTexto(elemento, texto, intervalo) {
 document.getElementById("h1-home").style.display = "none";
 const elementoTexto = document.getElementById('texto-escrevendo');
 const textoParaEscrever = 'LUCAS FERNANDES';
-const intervaloDeEscrita = 100;
+const intervaloDeEscrita = 150;
 escreverTexto(elementoTexto, textoParaEscrever, intervaloDeEscrita);
 
 
